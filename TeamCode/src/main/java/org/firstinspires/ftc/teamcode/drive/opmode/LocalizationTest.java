c8b00965612dc60a9e7f748a4ecad789bbe6d19d
Rohan Chandra <rc8yw@virginia.edu>
2023-03-29 12:15:24 -0400

    First commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -13,6 +13,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  * errors are not out of the ordinary, especially with sudden drive motions). The goal of this
  * exercise is to ascertain whether the localizer has been configured properly (note: the pure
  * encoder localizer heading may be significantly off if the track width has not been tuned).
+ *
  */
 @TeleOp(group = "drive")
 public class LocalizationTest extends LinearOpMode {
